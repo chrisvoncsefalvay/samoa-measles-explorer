6dd621d44276dfc7f8bc25022e5e8c96cdb78aa0
Chris Csefalvay <kcsefalvay@apple.com>
2019-12-03 00:19:50 +0100

    New memoization parameters instituted.

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ cache = Cache(app.server, config={
 
 
 # Loader
-#@cache.memoize(timeout=3600)
+@cache.memoize(timeout=600)
 def get_data() -> pd.DataFrame:
     data_url = "https://raw.githubusercontent.com/chrisvoncsefalvay/samoa-measles-2019/master/data/cumulative_data.csv"
     data = pd.read_csv(data_url,
@@ -43,7 +43,7 @@ location = dcc.Location(id="url", refresh=True)
 
 app.layout = html.Div([location, navbar, body])
 
-#@cache.memoize(600)
+@cache.memoize(600)
 @app.callback([dash.dependencies.Output("sidecart-contents", "children"),
                dash.dependencies.Output("plot-object", "children"),
                dash.dependencies.Output("last-update", "children")],
